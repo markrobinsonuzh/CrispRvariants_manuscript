4eb1d55b80cf04e35c491b3cc582dec2ca76815f
Helen Lindsay <helen.lindsay@uzh.ch>
2016-02-28 14:35:19 +0100

    deleted unnecessary files

--- src/run_mapping.R
+++ src/run_mapping.R
@@ -1,4 +1,3 @@
-
 map <- function(in_fq, out_prefix){
   index <- "../idx/danRer7.fa"
   cmd <- paste("bwa mem -t 12 %s %s | samtools view -Sb - > %s_temp.bam",
@@ -11,31 +10,31 @@ map <- function(in_fq, out_prefix){
 
 
 # Map merged reads
-#map("../fastq/SRR1769728_merged_seqprep.fastq.gz", "../bam/merged_seqprep")
+map("../fastq/SRR1769728_merged_seqprep.fastq.gz", "../bam/merged_seqprep")
 
 
 # Map merged split reads
 guides <- list.files("../merged_split")
-guides <- guides[! guides %in% c("tolerant_pear", "split_pear")]
+guides <- guides[! guides %in% c("tolerant_pear", "strict_pear")]
 
-#dummy <- lapply(guides, function(gd){
-#        pear_template <- "../merged_split/%s/SRR1769728_merged_pear.assembled.fastq.gz"
-#        seqprep_template <- "../merged_split/%s/SRR1769728_merged_seqprep.fastq.gz"
-#        seqprep55_template <- "../merged_split/%s/SRR1769728_merged_seqprep_l55.fastq.gz"
-#        pear_bam <- "../bam/merged_split_pear/%s"
-#        seqprep_bam <- "../bam/merged_split_seqprep/%s"
-#        seqprep55_bam <- "../bam/merged_split_l55_n90/%s"
-#        map(sprintf(pear_template, gd), sprintf(pear_bam, gd))
-#        map(sprintf(seqprep_template, gd), sprintf(seqprep_bam, gd))
-#        map(sprintf(seqprep55_template, gd), sprintf(seqprep55_bam, gd))
-#})
+dummy <- lapply(guides, function(gd){
+        pear_template <- "../merged_split/%s/SRR1769728_merged_pear.assembled.fastq.gz"
+        seqprep_template <- "../merged_split/%s/SRR1769728_merged_seqprep.fastq.gz"
+        seqprep55_template <- "../merged_split/%s/SRR1769728_merged_seqprep_l55.fastq.gz"
+        pear_bam <- "../bam/merged_split_pear/%s"
+        seqprep_bam <- "../bam/merged_split_seqprep/%s"
+        seqprep55_bam <- "../bam/merged_split_l55_n90/%s"
+        map(sprintf(pear_template, gd), sprintf(pear_bam, gd))
+        map(sprintf(seqprep_template, gd), sprintf(seqprep_bam, gd))
+        map(sprintf(seqprep55_template, gd), sprintf(seqprep55_bam, gd))
+})
 
 # Map split_merged (guides consistent across all conditions)
-#dummy <- lapply(guides, function(gd){
-#        pear_template <- "../split_merged/%s/SRR1769728_merged_pear.fastq.gz"
-#        pear_bam <- "../bam/split_merged_pear/%s"
-#        map(sprintf(pear_template, gd), sprintf(pear_bam, gd))
-#})
+dummy <- lapply(guides, function(gd){
+        pear_template <- "../split_merged/%s/SRR1769728_merged_pear.fastq.gz"
+        pear_bam <- "../bam/split_merged_pear/%s"
+        map(sprintf(pear_template, gd), sprintf(pear_bam, gd))
+})
 
 # Map primer comparison
 dummy <- lapply(guides, function(gd){
